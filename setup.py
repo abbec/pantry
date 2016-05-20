96e1d3e3ffd5f33257e9b1a3497a34fb40bf11ca
Albert Cervin <albert@acervin.com>
2016-05-20 21:40:27 +0200

    Add classifiers

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup
 setup(name='pantry',
       version='0.1',
       packages=['pantry'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Programming Language :: Python :: 3.5',
+          ],
       install_requires=[
           'Flask',
           'Flask-SQLAlchemy',
